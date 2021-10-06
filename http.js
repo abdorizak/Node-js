2da6e469a28c164de9719d3942c5f4cdcdb7695d
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-10-06 14:02:04 +0300

    Start Learning Node js

--- http.js
+++ http.js
@@ -1,1 +1,16 @@
-const http = require('http');
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("hellow world");
+    res.end();
+  }
+
+  if (req.url === "/api/courses") {
+    res.write(JSON.stringify([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]));
+    res.end();
+  }
+});
+const port = 3000;
+server.listen(port);
+
+console.log(`listening on port:${port}`);
