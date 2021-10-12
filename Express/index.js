b44bc7eb4ff95d337a9eb2ba6f38aff298802a2d
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-10-12 13:36:03 +0300

    Learning Node js

--- Express/index.js
+++ Express/index.js
@@ -64,6 +64,16 @@ app.put("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Not Found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
